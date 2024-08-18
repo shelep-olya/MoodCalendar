3e13b8120f843dd312cb3d4967f6e26b713724e2
shelep-olya <shelep.olya@gmail.com>
2024-08-18 23:14:59 +0300

    added login & signup layouts

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,3 +4,11 @@ const authLayout = "../views/layouts/auth.ejs";
 exports.getMainPage = (req, res) => {
     res.status(200).render("calendar", {layout: mainLayout});
 };
+
+exports.getSignupPage = (req, res) => {
+    res.status(200).render("signup", {layout: mainLayout});
+};
+
+exports.getLoginPage = (req, res) => {
+    res.status(200).render("login", {layout: mainLayout});
+};
